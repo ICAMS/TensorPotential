1702369d9b84051552bdee74566f9e1937ab84ba
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-04-11 16:05:06 +0200

    Reformat using black

--- setup.py
+++ setup.py
@@ -4,25 +4,22 @@ import versioneer
 
 
 setup(
-    name='tensorpotential',
-    #version=versioneer.get_version(),
-    version='0.2.0+19.gfa5b8b0',
+    name="tensorpotential",
+    # version=versioneer.get_version(),
+    version="0.2.0+19.gfa5b8b0",
     cmdclass=versioneer.get_cmdclass(),
-    packages=['tensorpotential', 'tensorpotential.utils', 'tensorpotential.potentials', 'tensorpotential.functions'],
-    package_dir={'': 'src'},
-    url='',
-    license='',
-    author='Anton Bochkarev',
-    author_email='',
-    description='',
-    python_requires='<3.10',
-    install_requires=
-    [
-    'scipy',
-    'tensorflow<=2.9.1',
-    'numpy',
-    'pandas',
-    'ase'
-    ]
+    packages=[
+        "tensorpotential",
+        "tensorpotential.utils",
+        "tensorpotential.potentials",
+        "tensorpotential.functions",
+    ],
+    package_dir={"": "src"},
+    url="",
+    license="",
+    author="Anton Bochkarev",
+    author_email="",
+    description="",
+    python_requires="<3.10",
+    install_requires=["scipy", "tensorflow<=2.9.1", "numpy", "pandas", "ase"],
 )
-
