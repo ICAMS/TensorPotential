d5b2bb40c6360fc839baf489ae81ac926b3ac879
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2023-04-11 16:12:04 +0200

    Modify to install tensorflow-macos for M1/M2 Mac

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
+import platform
+
 from setuptools import setup
 
 import versioneer
+
+install_requires = [
+    "scipy",
+    "numpy",
+    "pandas",
+    "ase",
+]
+if platform.system() == "Darwin" and platform.machine() == "arm64":
+    # M1/M2 Mac
+    install_requires.append("tensorflow-macos<=2.9.1")
+else:
+    install_requires.append("tensorflow<=2.9.1")
 
 
 setup(
@@ -21,5 +35,5 @@ setup(
     author_email="",
     description="",
     python_requires="<3.10",
-    install_requires=["scipy", "tensorflow<=2.9.1", "numpy", "pandas", "ase"],
+    install_requires=install_requires,
 )
